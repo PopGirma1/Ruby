c63d6ec020ea33b906d16f7538caaf8d4f656137
Girma <45471674+PopGirma1@users.noreply.github.com>
2019-12-03 23:40:29 +0300

    mvc5

---
+++ MVC5/WebApplication11/Controllers/DemoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class DemoController : Controller
+    {
+        mvcEntities db = new mvcEntities();
+        // GET: Demo
+        public ActionResult Index()
+        {
+            List<tbl_employee> emplist = db.tbl_employee.ToList();
+
+          
+            List<employeeviewmodel> evmlist = emplist.Select(x => new employeeviewmodel
+            {
+                emp_id = x.emp_id,
+                emp_name = x.emp_name,
+                emp_hiredate = x.emp_hiredate,
+                emp_img = x.emp_img,
+                emp_dob = x.emp_dob,
+                dep_id = Convert.ToInt32(x.emp_fk_id),
+                dep_name = x.tbl_department.dep_name
+            }).ToList();
+
+
+            return View(evmlist);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+}
---
+++ MVC5/WebApplication11/Models/employeeviewmodel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication11.Models
+{
+    public class employeeviewmodel
+    {
+        public int emp_id { get; set; }
+
+
+        [Required(ErrorMessage="*")]
+
+        public string emp_name { get; set; }
+        [Required(ErrorMessage = "*")]
+        public System.DateTime emp_dob { get; set; }
+        public System.DateTime emp_hiredate { get; set; }
+        public string emp_img { get; set; }
+
+
+        public int dep_id { get; set; }
+  [Required(ErrorMessage = "*")]
+
+        public string dep_name { get; set; }
+
+
+
+    }
+}
